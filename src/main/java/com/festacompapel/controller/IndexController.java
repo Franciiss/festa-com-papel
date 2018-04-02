88667b780966c7486cfe61763d595c892546252c
Francisco <Francisco.Carreiro96@gmail.com>
2018-04-02 18:45:06 -0300

    paginas

--- src/main/java/com/festacompapel/controller/IndexController.java
+++ src/main/java/com/festacompapel/controller/IndexController.java
@@ -5,22 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.festacompapel.model.Cliente;
-import com.festacompapel.repositry.ClienteRepository;
-
 @Controller
 public class IndexController {
-
-	@Autowired
-	ClienteRepository clienteRepository;
 
 	@RequestMapping("/welcome")
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView("/index");
-		
-		Cliente c = new Cliente();
-		c.setNome("Francisco");
-		clienteRepository.save(c);
+
 		return modelAndView;
 	}
 
