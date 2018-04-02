2f82a313b948a7c1374b459ca5ca62e5ecea0b1a
Francisco <Francisco.Carreiro96@gmail.com>
2018-04-02 17:33:40 -0300

    commit inicial

--- src/main/java/com/festacompapel/config/DataConfiguration.java
+++ src/main/java/com/festacompapel/config/DataConfiguration.java
@@ -1,35 +1,28 @@
 package com.festacompapel.config;
 
-import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class DataConfiguration {
 
 	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("us-cdbr-iron-east-05.cleardb.net");
-		dataSource.setUsername("b7aa60b7e27f66");
-		dataSource.setPassword("765a7aac4cab7cf");
-		return dataSource;
-	}
+	public BasicDataSource dataSource() throws URISyntaxException {
+		URI dbUri = new URI(System.getenv("mysql://b7aa60b7e27f66:93d5da78@us-cdbr-iron-east-05.cleardb.net/heroku_4159ac44678b0c4?reconnect=true"));
 
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter() {
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.MYSQL);
-		adapter.setShowSql(true);
-		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL57Dialect");
-		adapter.setPrepareConnection(true);
-		return adapter;
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setUrl(dbUrl);
+		basicDataSource.setUsername(username);
+		basicDataSource.setPassword(password);
+
+		return basicDataSource;
 	}
 }
